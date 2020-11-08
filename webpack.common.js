a9af5064608ce79374330fb1549991925b3e2e47
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-09 00:20:01 +0700

    optimization config

--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,24 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+      minSize: 20000,
+      maxSize: 46000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
   },
   module: {
