f9796d098c97c80f1fde6fc079e46184108e2f2b
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-10 15:27:57 +0700

    change card menu component icons

--- src/scripts/components/MenuCardComponent.js
+++ src/scripts/components/MenuCardComponent.js
@@ -2,6 +2,8 @@
 import '../../styles/sass/MenuCardComponent.scss';
 import ListIcon from '../../public/icons/ListIcon';
 import ListIconSecondary from '../../public/icons/ListIconSecondary';
+import foodIcon from '../../public/icons/fastfood-24px.svg';
+import drinkIcon from '../../public/icons/local_bar-24px.svg';
 
 class MenuCardComponent extends HTMLElement {
   _capitalizeFirstLetter(string) {
@@ -30,7 +32,7 @@ class MenuCardComponent extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="menu__wrap ${this._name}">
-        <h3 class="menu__title"><span class="material-icons">${this._name === 'foods' ? 'fastfood' : 'local_bar'}</span> ${this._capitalizeFirstLetter(this._name)}</h3>
+        <h3 class="menu__title">${this._name === 'foods' ? `<img class="material-icons" src="${foodIcon}" alt="Food Icon" />` : `<img class="material-icons" src="${drinkIcon}" alt="Drink Icon" />`}</img> ${this._capitalizeFirstLetter(this._name)}</h3>
         <ul class="menu__list"></ul>
       </div>
     `;
