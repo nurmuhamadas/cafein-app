356636b14f24459d354f9682a726f3001cc308d2
Nur Muhamad Ash Shidiqi <nurmuhamad.a.13@gmail.com>
2020-11-11 13:36:41 +0700

    Create skeleton at detail component

--- src/scripts/components/DetailComponent.js
+++ src/scripts/components/DetailComponent.js
@@ -2,22 +2,44 @@ import '../../styles/sass/DetailComponent.scss';
 import locationIcon from '../../public/icons/location_on-24px.svg';
 
 class DetailComponent extends HTMLElement {
+  constructor() {
+    super();
+
+    this._isLoading = true;
+  }
+
   _insertCategory(category) {
     this.querySelector('.restaurant__category').innerHTML += `<p class="restaurant__category-pill">${category.name}</p>`;
   }
 
   set data(restaurant) {
     this._restaurant = restaurant;
+
+    this._isLoading = !restaurant;
   }
 
   async connectedCallback() {
     this.render();
 
-    this._restaurant.categories.forEach((category) => this._insertCategory(category));
+    if (!this._isLoading) {
+      this._restaurant.categories.forEach((category) => this._insertCategory(category));
+    }
   }
 
   render() {
-    this.innerHTML = `
+    this.innerHTML = this._isLoading
+      ? `
+      <div id="detail-restaurant" class="skeleton">
+        <div class="restaurant__img-wrap"></div>
+        <div class="restaurant__detail">
+          <div class="restaurant__category"></div>
+          <div class="restaurant__label"></div>
+          <h2 class="restaurant__name"></h2>
+          <p class="restaurant__description"></p>
+        </div>
+      </div>
+    `
+      : `
       <div id="detail-restaurant">
         <div class="restaurant__img-wrap">
           <img class="restaurant__img-item" src="${this._restaurant.pictureUrl}" alt="${this._restaurant.name}">
